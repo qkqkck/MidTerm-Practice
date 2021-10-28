120f72c9636620d3d8bd3c6f61411eb8867193aa
20171221 민석 <90017906+qkqkck@users.noreply.github.com>
2021-10-28 12:25:24 +0000

    Day 1

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@ SDL_Renderer* g_pRenderer = 0;
 int main(int argc, char* args[])
 {
  
-   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+   if (SDL_Init(SDL_INIT_EVERYTHING) == 0) 
+   {
 
-        g_pWindow = SDL_CreateWindow("Setting up SDL 10/22",
+        g_pWindow = SDL_CreateWindow("TesT SDL",
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
-            640, 480, SDL_WINDOW_SHOWN);
+            512, 512, SDL_WINDOW_SHOWN);
 
-        if (g_pWindow != 0) {
+        if (g_pWindow != 0) 
+        {
             g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
         }
 
@@ -22,7 +24,7 @@ int main(int argc, char* args[])
         return 1;
     }
 
-    SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
     SDL_RenderClear(g_pRenderer);
     SDL_RenderPresent(g_pRenderer);
 
